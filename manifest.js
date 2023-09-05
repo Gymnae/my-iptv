7d2edf1c4a55f64ce1e535d4f7e5049465975f65
Gunnar Falk <gunnar@grundstil.de>
2023-09-05 20:49:46 +0200

    first

---
+++ manifest.js
@@ -0,0 +1,14 @@
+module.exports={
+    id: "org.community.myIPTV",
+    version: "0.0.1",
+    name: "My IPTV",
+    logo: "https://dl.strem.io/addon-logo.png",
+    description: "This addon brings all the Live Streams, VOD streams and Series from your IPTV subscription to your Stremio 
+using Xtream API.",
+    types: ["movie", "series","tv","channel"],
+    background: "https://dl.strem.io/addon-background.jpg",
+    resources: ["movie","series","tv"],
+    catalogs: [],
+    idPrefixes: ["yiptv:"],
+    behaviorHints:{configurable : true, configurationRequired: true },
+};
