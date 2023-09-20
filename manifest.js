058faa4f22a1a8715a4d5cdb87cfc0619e2ed325
Gunnar Falk <gunnar@grundstil.de>
2023-09-20 09:51:10 +0200

    first

--- manifest.js
+++ manifest.js
@@ -1,6 +1,6 @@
 module.exports={
     id: "org.community.myIPTV",
-    version: "0.0.1",
+    version: "0.0.2",
     name: "My IPTV",
     logo: "https://dl.strem.io/addon-logo.png",
     description: "This addon brings all the Live Streams, VOD streams and Series from your IPTV subscription to your Stremio using Xtream API.",
