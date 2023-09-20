79a2df1eb1c079f8847e3a55ead5de8acf689b5b
Gunnar <dagorwaith@gmail.com>
2023-09-20 11:11:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,163 +1,347 @@
+const axios = require('axios').default;
+axios.defaults.headers.get["content-type"] = "application/json";
+axios.defaults.timeout = 80000
+axios.defaults.method = "GET"
 
-const express = require("express");
-const app = express();
-const config = require('./config');
-const MANIFEST = require('./manifest');
-const { getManifest, getCatalog, getMeta, getUserData } = require("./addon");
+function getUserData(userConf) {
 
-const NodeCache = require( "node-cache" );
-const myCache = new NodeCache({stdTTL:15*60});
-
-var respond = function (res, data) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-  };
-  
-// app.use(express.static('public'))
-
-app.engine('html', require('ejs').renderFile);
-app.set('views', __dirname);
-
-app.get("/", function (req, res) {
-  res.redirect("/configure")
-});
-
-app.get("/:userConf?/configure", function (req, res) {
-  const newManifest = { ...{MANIFEST} };
-  res.render('configure.html',newManifest);
-  // res.render('configure.html',{MANIFEST});
-});
-
-app.get('/manifest.json', function (req, res) {
-  const newManifest = { ...MANIFEST };
-  newManifest.behaviorHints.configurationRequired = true;
-  respond(res, newManifest);
-});
-
-app.get('/:userConf/manifest.json', async function (req, res) {
-  let newManifest = { ...MANIFEST };
-  if (!((req || {}).params || {}).userConf) {
-      newManifest.behaviorHints.configurationRequired = true;
-      respond(res, newManifest);
-  }else {
+    let retrievedData, url, obj = {}
     try {
+        retrievedData = JSON.parse(Buffer.from(userConf, 'base64').toString())      
+    } catch (error) {
+        console.log(error)
+        return "error while parsing url"
+    }
+    
+    let domainName,baseURL,idPrefix
 
-      if (myCache.has(`manifest-${req.params.userConf}`)) {
-        respond(res,myCache.get(`manifest-${req.params.userConf}`))
-      }else{
-        newManifest = await getManifest(req.params.userConf)
-        // newManifest.behaviorHints.configurationRequired = false;
-        if(newManifest && newManifest.id){
-          myCache.set(`manifest-${req.params.userConf}`,newManifest)
+    if(typeof retrievedData === "object"){
+        domainName = retrievedData.BaseURL.split("/")[2].split(":")[0] || "unknown"
+        baseURL = retrievedData.BaseURL
+        idPrefix = domainName.charAt(0) + domainName.substr(Math.ceil(domainName.length / 2 - 1), domainName.length % 2 === 0 ? 2 : 1) + domainName.charAt(domainName.length - 1) + ":";
+        
+        obj = {
+            baseURL,
+            domainName,
+            idPrefix,
+            username:retrievedData.username,
+            password:retrievedData.password
         }
-        respond(res, newManifest);
-      }  
-    } catch (error) {
-      console.log(error)
-      respond(res,{error:error})
+
+    }else if(retrievedData.includes("http")){
+        url = retrievedData
+        
+        const queryString = url.split('?')[1] || "unknown"
+        baseURL = url.split('/')[0] + "//" + url.split('?')[0].split('/')[2] || "unknown"
+        
+        domainName = url.split("?")[0].split("/")[2].split(":")[0] || "unknown"
+        idPrefix = domainName.charAt(0) + domainName.substr(Math.ceil(domainName.length / 2 - 1), domainName.length % 2 === 0 ? 2 : 1) + domainName.charAt(domainName.length - 1) + ":";
+        
+        if(queryString === undefined){return {result:"URL does not have any queries!"}}
+        if(baseURL === undefined){return {result:"URL does not seem like an url!"}}
+        
+        obj.baseURL = baseURL
+        obj.domainName = domainName
+        obj.idPrefix = idPrefix
+        
+        const urlParams = new URLSearchParams(queryString);
+        const entries = urlParams.entries();
+        
+        for(const entry of entries) {
+            obj[entry[0]] = entry[1]
+        }
     }
-  }
-});
 
-app.get('/:userConf/catalog/:type/:id/:extra?.json', async function (req, res) {
-  let {userConf,type,id,extra} = req.params
-  let extraObj
-
-  if(extra){
-    try {
-      extraObj = JSON.parse('{"' + decodeURI(extra.replace(/&/g, "\",\"").replace(/=/g,"\":\"")) + '"}')
-    } catch (error) {
-      console.log(error)
-      return respond(res, {metas:[]})
-    }
-  }
-  
-  if(extraObj && extraObj.genre && extraObj.genre.includes("+")){
-    extraObj.genre = extraObj.genre.replace(/\+/g,' ')
-  }
-
-  let metas = []
-  try {
-
-    if (myCache.has(`catalog-${userConf}-${type}-${id}-${extra}`)) {
-      respond(res,myCache.get(`catalog-${userConf}-${type}-${id}-${extra}`))
+    if(obj.username && obj.password && obj.baseURL){
+        return obj
     }else{
-      metas = await getCatalog(userConf,type,extraObj.genre)
-      if(metas.length > 0){
-        myCache.set(`catalog-${userConf}-${type}-${id}-${extra}`,{metas: metas})
-      }
-      respond(res, {metas: metas})
+        console.log("Error while parsing!")
+        return {}
     }
-
-  } catch (error) {
-    console.log(error)
-    respond(res, {metas:[]})
-    
-  }
-    
-});
-
-app.get('/:userConf/meta/:type/:id.json', async function (req,res){
-
-  let {userConf,type,id} = req.params
-
-  try {
-    if (myCache.has(`meta-${userConf}-${type}-${id}`)) {
-      respond(res,myCache.get(`meta-${userConf}-${type}-${id}`))
-    }else{
-      const meta = await getMeta(userConf,type,id)
-      if(meta && meta.id){
-        myCache.set(`meta-${userConf}-${type}-${id}`,{meta:meta})
-      }
-      respond(res, {meta:meta})
-    }
-
-  } catch (error) {
-    console.log(error)
-    respond(res,{error})
-  }
-});
-
-
-app.get('/:userConf/stream/:type/:id.json', function (req, res) {
-
-  let {userConf,type,id} = req.params
-
-  let extension = "mp4"
-  const obj = getUserData(userConf)
-  const streamID = id.split(":")[1]
-  let stream = []
-  if(type === "tv"){
-    type = "live";
-    extension = "m3u8";
-    stream = [{
-      url: `${obj.baseURL}/${type}/${obj.username}/${obj.password}/${streamID}.${extension}`,
-      description:"Watch Now",
-      behaviorHints :{
-        notWebReady:true
-      }
-    }]
-  }else{
-    stream = [{
-      url: `${obj.baseURL}/${type}/${obj.username}/${obj.password}/${streamID}.${extension}`,
-      description:"Watch Now"
-    }]
-  }
-
-  try {
-    respond(res,{streams:stream})
-  } catch (error) {
-    console.log(error)
-    respond(res,{streams:[]})
-  }
-});
-
-if (module.parent) {
-  module.exports = app;
-} else {
-  app.listen(config.port, function () {
-  console.log(config)
-});
 }
+async function getManifest(url) {
+    const obj = getUserData(url)
+
+    let vod
+    try {
+        vod = await axios({url:`${obj.baseURL}/player_api.php?username=${obj.username}&password=${obj.password}&action=get_vod_categories`})
+    } catch (error) {
+        console.log(error)
+        return {error}
+    }
+    const vodJSON = vod.data
+
+    let movieCatalog = []
+        if (vod.status === 200){    
+        vodJSON.forEach(i => {
+            let name = i.category_name
+            movieCatalog.push(name)
+        });
+    }
+    let series
+    try {
+        series = await axios({url:`${obj.baseURL}/player_api.php?username=${obj.username}&password=${obj.password}&action=get_series_categories`})
+    } catch (error) {
+        console.log(error)
+        return {error}
+    }
+    const seriesJSON = series.data
+
+    let seriesCatalog = []
+    if(series.status === 200){    
+        seriesJSON.forEach(i => {
+            let name = i.category_name
+            seriesCatalog.push(name)
+        });
+    }
+let live
+    try {
+        live = await axios({url:`${obj.baseURL}/player_api.php?username=${obj.username}&password=${obj.password}&action=get_live_categories`})
+    } catch (error) {
+        console.log(error)
+        return {error}
+    }
+    const liveJSON = live.data
+
+    let liveCatalog = []
+    if(series.status === 200){    
+        liveJSON.forEach(i => {
+            let name = i.category_name
+            liveCatalog.push(name)
+        });
+    }
+    const manifest = {
+        id:`org.community.${obj.domainName}` || "org.community.youriptv",
+        version:"1.0.0",
+        name:obj.domainName + " IPTV" || "Your IPTV",
+        description:`You will access to your ${obj.domainName} IPTV with this addon!`,
+        idPrefixes:[obj.idPrefix],
+        //idPrefixes:["tmdb:", obj.idPrefix],
+        catalogs:[
+            {
+                id:`${obj.idPrefix}movie`,
+                name: obj.domainName || "Your IPTV",
+                type:"movie",
+                extra:[{name:"genre",options:movieCatalog,isRequired:true}],
+                isRequired: true
+            },
+            {
+                id:`${obj.idPrefix}series`,
+                name:obj.domainName || "Your IPTV",
+                type:"series",
+                extra:[{name:"genre",options:seriesCatalog,isRequired:true}],
+                isRequired: true
+            },
+            {
+                id:`${obj.idPrefix}tv`,
+                name: obj.domainName || "Your IPTV",
+                type:"tv",
+                extra:[{name:"genre",options:liveCatalog,isRequired:true}],
+                
+            }
+        ],
+        resources:["catalog","meta","stream"],
+        types:["movie","series","tv"],
+        behaviorHints:{configurable : true, configurationRequired: false },
+    }
+
+    return manifest
+    
+}
+
+function getValidUrl(url) {
+    try {
+        const urlObj = new URL(url);
+        return urlObj.protocol.startsWith('http') ? url : '';
+    } catch {
+        return '';
+    }
+}
+
+async function getCatalog(url,type,genre) {
+
+    const obj = getUserData(url)
+
+   let getCategoryID
+
+    try { 
+        if(type === "movie"){
+        getCategoryID = await axios({url:`${obj.baseURL}/player_api.php?username=${obj.username}&password=${obj.password}&action=get_vod_categories`})    
+        } 
+        else if(type ==="series"){
+        getCategoryID = await axios({url:`${obj.baseURL}/player_api.php?username=${obj.username}&password=${obj.password}&action=get_series_categories`})    
+        } 
+        else if(type ==="tv"){
+            getCategoryID = await axios({url:`${obj.baseURL}/player_api.php?username=${obj.username}&password=${obj.password}&action=get_live_categories`})    
+        }
+    }catch (error) {
+        console.log(error)
+        return []
+    }
+   
+    let catID
+    
+    getCategoryID.data.forEach(i => {
+        if(i.category_name === genre){
+            catID = i.category_id
+        }
+    });
+    // console.log(catID)
+
+    let action = type === "movie" ? "get_vod_streams" : type === "series" ? "get_series": type ==="tv" ? "get_live_streams" : "error"
+
+    let paramsCat = {
+        username: obj.username,
+        password: obj.password,
+        action,
+        category_id: catID
+    }
+
+    let getCatalogs
+    try {
+        getCatalogs = await axios({url:`${obj.baseURL}/player_api.php`,method:"GET",params:paramsCat})
+    } catch (error) {
+        console.log(error)
+        // return [{error,errorLocation:"Could not use the parameters wile searching catalogs!"}]
+        return []
+    }
+
+    let metas = []
+
+    getCatalogs.data.forEach(i => {
+        let id,name = i.name, poster, posterShape, imdbRating
+
+        if(type === "series"){
+            id = obj.idPrefix + i.series_id || ""
+            poster = getValidUrl(i.cover)
+            imdbRating = i.rating || ""
+            posterShape = "poster"
+        }else if(type === "movie"){
+            id = obj.idPrefix + i.stream_id || ""
+            poster = getValidUrl(i.stream_icon)
+            imdbRating = i.rating || ""
+            posterShape = "poster"
+        }else if (type === "tv"){
+            id = obj.idPrefix + i.stream_id || ""
+            poster = getValidUrl(i.stream_icon)
+            imdbRating = null
+            posterShape = "square"
+        }
+
+        metas.push({id,type,name,poster,posterShape,imdbRating})
+    }); 
+
+    return metas
+}   
+async function getMeta(url,type,id) {
+
+    const streamID = id.split(":")[1]
+
+    const obj = getUserData(url)
+
+    let action = type === "movie" ? "get_vod_info" : type === "series" ? "get_series_info": type ==="tv" ? "get_live_streams" : "error"
+    let requestID = type === "movie" ? "vod_id" : type === "series" ? "series_id": type ==="tv" ? "stream_id" : "error"
+
+    let params = {
+        username: obj.username,
+        password: obj.password,
+        action,
+        [requestID]:streamID
+    }
+
+    if(type === "tv"){
+        delete params[requestID]
+    }
+    // console.log(params)
+
+    let getMeta
+
+    try {
+        getMeta = await axios({url:`${obj.baseURL}/player_api.php`,params})
+    } catch (error) {
+        console.log(error)
+        return {}
+        // return [{error,errorLocation:"Could not use the parameters wile searching meta!"}]
+    }
+
+    // console.log(getMeta.data)
+
+    let meta = {}
+
+    if(type === "movie"|| type === "series"){
+        meta ={
+             id: obj.idPrefix + streamID || "",
+             //id: getMeta.data.info.tmdb_id === undefined ? obj.idPrefix + streamID : "tmdb:"+getMeta.data.info.tmdb_id, // "tmdb:"+getMeta.data.info.tmdb_id || obj.idPrefix + streamID || "",
+            type,
+            name: getMeta.data.info.name === undefined ? "" : getMeta.data.info.name,
+            poster: getMeta.data.info.cover_big || "",
+            background: getMeta.data.info.backdrop_path[0] || "https://www.stremio.com/website/wallpapers/stremio-wallpaper-5.jpg",
+            description: getMeta.data.info.description || "",
+            releaseInfo: String(getMeta.data && getMeta.data.info && (getMeta.data.info.releaseDate || getMeta.data.info.releasedate).split("-")[0])
+        }
+    }
+       
+    if(type === "series"){
+        let videos = []
+       
+        const seasons = Object.keys(getMeta.data.episodes)
+
+        seasons.forEach(season => {
+            // console.log(season)
+            getMeta.data.episodes[season].forEach(episode => {
+                let id = obj.idPrefix + episode.id || ""
+                let title = episode.title || ""
+                let season = episode.season || null
+                let episodeNo = episode.episode_num || null
+                let overview = episode.plot || ""
+                let thumbnail = episode.info.movie_image || null
+                let released = episode.info.releasedate === undefined ? Date.now() : new Date(episode.info.releasedate)
+                let container_extension = episode.container_extension || "mp4"
+
+                let streams = [{
+                    name:obj.domainName,
+                    description: title,
+                    url: `${obj.baseURL}/series/${obj.username}/${obj.password}/${episode.id}.${container_extension}`,
+                }]
+
+                // console.log(streams)
+                videos.push({id,title,season,episode:episodeNo,overview,thumbnail,released,streams})
+            });
+        });
+
+        // console.log(videos)
+        meta.name = getMeta.data.info.name || "",
+        meta.videos = videos
+
+    }else if(type === "movie"){
+
+        let imdbRating = getMeta.data.info.rating || ""
+        meta.imdbRating = imdbRating
+        
+    }else if(type === "tv"){
+
+        let metaTV = []
+        getMeta.data.forEach(i => {
+            
+            if(Number(i.stream_id) === Number(streamID)){
+
+                let id= obj.idPrefix + i.stream_id
+                
+                let name = i.name || ""
+                let background=  "https://www.stremio.com/website/wallpapers/stremio-wallpaper-5.jpg"//i.stream_icon,
+                let logo =  i.stream_icon || null
+
+                metaTV.push({id,name,type,background,logo})
+                // console.log(metaTV)
+            }
+        });
+        // return metaTV
+        // console.log(metaTV)
+        return metaTV[0]
+    }
+
+    // console.log(meta)
+
+    return meta
+}
+module.exports={getUserData,getManifest,getCatalog,getMeta}
